4c11eea62dc68f9298ccf4cbee7783f8eb5e4c21
Olga Modina <o.modina@finiki.games>
2024-02-28 01:56:51 +0500

    Add Links.Api

---
+++ Links/Links.API/Controllers/LinksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Links.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LinksController : ControllerBase
+{
+    private readonly ILogger<LinksController> _logger;
+
+    public LinksController(ILogger<LinksController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public ActionResult<string> Get() => Ok("Hello World!");
+}
